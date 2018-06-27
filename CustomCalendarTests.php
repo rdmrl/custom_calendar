3731760479e8c6739c560b66b2a08bf162165db3
Ravi Damarla <ravi@endofthenight.com>
2018-06-27 15:49:00 +0000

    removed test for leap year - not needed

--- CustomCalendarTests.php
+++ CustomCalendarTests.php
@@ -25,20 +25,6 @@ class CustomCalendarTests extends TestCase
         $this->customCalendar = new CustomCalendar();
     }
 
-    /**
-     * Tests for a leap year using the custom calendar rules.
-     */
-    public function testLeapYear()
-    {
-        // A success test for a leap year.
-        $isALeapYear = 1990;
-        $this->assertTrue($this->customCalendar->isLeapYear($isALeapYear));
-
-        // A failure test for a leap year.
-        $notALeapYear = 1991;
-        $this->assertFalse($this->customCalendar->isLeapYear($notALeapYear));
-    }
-
     public function testInvalidDate()
     {
         $invalidDate = '01/01/2018';
@@ -51,20 +37,9 @@ class CustomCalendarTests extends TestCase
         $retInvalidDate2 = $this->customCalendar->getDayOfWeek($invalidDate2);
         $this->assertNull($retInvalidDate2);
 
-        $invalidDate3 = '01-01-xxxx';
+        $invalidDate3 = '01.01.xxxx';
 
         $retInvalidDate3 = $this->customCalendar->getDayOfWeek($invalidDate3);
         $this->assertNull($retInvalidDate3);
-    }
-
-    public function testDayOfWeek()
-    {
-        // Test the day of the week for the known value - Jan 1st 1990.
-        $testDate1 = '01.01.1990';
-        $retValueDate1 = $this->customCalendar->getDayOfWeek($testDate1);
-        $this->assertNotNull($retValueDate1, "The returned value from getDayOfWeek for " . $testDate1);
-
-        $dayOfWeekDate1 = 'Monday';
-        $this->assertEquals($dayOfWeekDate1, $retValueDate1);
     }
 }
